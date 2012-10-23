7c1139ee39fd9c30db83e1935b3d15e1b7f9c89a
Chad Engler <englercj@live.com>
2012-10-22 22:15:56 -0400

    Sprite updates

--- js/game/lib/core/Sprite.js
+++ js/game/lib/core/Sprite.js
@@ -12,8 +12,9 @@ define([], function() {
                 animations: {
                     name: { //semantic name
                         offset: [Number (X), Number (Y)], //offset to first frame in texture
-                        frames: Number, //default: 1 (no animation)
+                        numFrames: Number, //default: 1 (no animation)
                         area: [Number (C), Number (R)] //number of columns and rows the animation resides in, default: [frames, 1]
+                        duration: Number, //default: 1000
                         loop: Boolean, //default: true
                     },
                     ...
@@ -35,15 +36,22 @@ define([], function() {
             for(var a in opts.animations) {
                 this.animations[a] = {
                     offset: opts.animations[a].offset,
-                    frames: opts.animations[a].frames || 1; //number of frames
-                    area: opts.animations[a].area || [(opts.animations[a].frames || 1), 1]; //area (cols/rows of the sprite texture animation frames)
+                    numFrames: opts.animations[a].numFrames || 1, //number of frames
+                    area: opts.animations[a].area || [(opts.animations[a].numFrames || 1), 1], //area (cols/rows of the sprite texture animation frames)
+                    duration: opts.animations[a].duration || 1000, //duration of the animation
                     loop: opts.animations[a].loop || true;
                 };
+
+                this.animations[a]._frameTime = (this.animations[a].duration / this.animations[a].numFrames);
             }
 
             //misc
             this.affectedByDistance = opts.affectedByDistance || false;
             this.useScreenCoordinates = opts.useScreenCoordinates || false;
+
+            //class vars
+            this.currentFrameTime = 0;
+            this.currentTile = 0;
 
             //actual sprite
             this._sprite = new THREE.Sprite({
@@ -55,8 +63,33 @@ define([], function() {
         setPosition: function(x, y) {
             this.sprite.position.set(x, y, 0);
         },
-        update: function() {
+        setAnimation: function(name) {
+            if(this.animations[name]) {
+                this.currentFrameTime = 0;
+                this.currentTile = 0;
+                this.currentAnim = this.animations[name];
+                return this.currentAnim;
+            }
 
+            return null;
+        },
+        draw: function(delta) {
+            var ms = delta * 1000;
+
+            this.currentFrameTime += ms;
+            while(this.currentFrameTime > this.currentAnim._frameTime) {
+                this.currentFrameTime -= this.currentAnim._frameTime;
+                this.currentTile++;
+
+                if(this.currentTile == this.currentAnim.numFrames && this.currentAnim.loop)
+                    this.currentTile = 0;
+
+                var currColumn = this.currentTile % this.currentAnim.area[0];
+                this.texture.offset.x = currColumn / this.currentAnim.area[0];
+
+                var currRow = Math.floor(this.currentTile / this.currentAnim.area[0]);
+                this.texture.offset.y = currRow / this.currentAnim.area[1];
+            }
         }
     });
 });
