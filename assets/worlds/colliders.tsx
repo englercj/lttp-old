c6d283309dd91b038532a5180be458f703562509
Chad Engler <englercj@live.com>
2012-12-04 08:02:57 -0500

    Add some map assets

---
+++ assets/worlds/colliders.tsx
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="colliders" tilewidth="16" tileheight="16">
+ <image source="colliders.png" width="160" height="112"/>
+ <tile id="0">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="normal" value="1|-1"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="normal" value="-1|-1"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="normal" value="1|1"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="isCollidable" value="true"/>
+   <property name="normal" value="-1|1"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+</tileset>
