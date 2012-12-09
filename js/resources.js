6f29935382a869826947a3cc95b14c51448721ba
Chad Engler <englercj@live.com>
2012-12-09 09:50:58 -0500

    Moved resources to seperate file

---
+++ js/resources.js
@@ -0,0 +1,24 @@
+define([], function() {
+    return [
+        {
+            name: 'darkworld_music',
+            type: 'audio',
+            src: '/assets/audio/music/dark_world.lite.ogg'
+        },
+        {
+            name: 'lightworld_world',
+            type: 'world',
+            src: '/assets/worlds/lightworld/lightworld.json'
+        },
+        {
+            name: 'darkworld_world',
+            type: 'world',
+            src: '/assets/worlds/darkworld/darkworld.json'
+        },
+        {
+            name: 'link_sprite',
+            type: 'texture',
+            src: '/assets/entities/link/link.png'
+        }
+    ];
+});
