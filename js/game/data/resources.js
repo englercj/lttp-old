fc80dc58fa0912229f4a96ad6e71e12264b104b5
Chad Engler <englercj@live.com>
2012-12-10 14:41:43 -0500

    rearrange some dataz

--- js/game/data/resources.js
+++ js/game/data/resources.js
@@ -2,22 +2,22 @@ define([], function() {
     return [
         {
             name: 'darkworld_music',
-            type: 'audio',
+            type: gf.types.RESOURCE.AUDIO,
             src: '/assets/audio/music/dark_world.lite.ogg'
         },
         {
             name: 'lightworld_world',
-            type: 'world',
+            type: gf.types.RESOURCE.WORLD,
             src: '/assets/worlds/lightworld/lightworld.json'
         },
         {
             name: 'darkworld_world',
-            type: 'world',
+            type: gf.types.RESOURCE.WORLD,
             src: '/assets/worlds/darkworld/darkworld.json'
         },
         {
             name: 'link_sprite',
-            type: 'texture',
+            type: gf.types.RESOURCE.TEXTURE,
             src: '/assets/entities/link/link.png'
         }
     ];
