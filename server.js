aee901318b182736397fe0808efb12c8e3add5f8
Chad Engler <englercj@live.com>
2013-01-02 11:39:58 -0500

    Minor server changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express'),
-    app = express();
-
+    app = express().
+    PORT = 3001;
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname));
 app.use(app.router);
 
-app.listen(3001);
+app.listen(PORT, function() {
+    console.log('listening on localhost:'+PORT);
+});
