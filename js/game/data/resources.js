dc2bccb76a5a72c8dee9395cdcca5f5553beb0f7
Chad Engler <englercj@live.com>
2013-01-24 16:49:16 -0500

    add lightworld music resource

--- js/game/data/resources.js
+++ js/game/data/resources.js
@@ -6,6 +6,11 @@ define([], function() {
             type: gf.types.RESOURCE.WORLD,
             src: '/assets/worlds/lightworld/lightworld.json'
         },
+        {
+            name: 'music_lightworld',
+            type: gf.types.RESOURCE.MUSIC,
+            src: '/assets/audio/music/overworld.lite.ogg'
+        },
         //Darkworld
         {
             name: 'world_darkworld',
