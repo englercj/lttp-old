2cc522f43bfdfb96d3c43b4c8d71ad45e6d9ef03
Chad Engler <englercj@live.com>
2013-08-01 08:26:33 -0700

    add link's house map

--- assets/worlds/shared_tiles/colliders.tsx
+++ assets/worlds/shared_tiles/colliders.tsx
@@ -1,66 +1,93 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Colliders" tilewidth="16" tileheight="16">
- <image source="colliders.png" width="160" height="112"/>
+ <image source="colliders.png" width="256" height="128"/>
  <tile id="0">
   <properties>
-   <property name="collidable" value="true"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="collidable" value="true"/>
-   <property name="half" value="yup"/>
    <property name="hitArea" value="[0,0,8,16]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
-   <property name="collidable" value="true"/>
    <property name="hitArea" value="[8,0,8,16]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
-   <property name="collidable" value="true"/>
    <property name="hitArea" value="[0,0,16,8]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
-   <property name="collidable" value="true"/>
    <property name="hitArea" value="[0,8,16,8]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="collidable" value="true"/>
    <property name="hitArea" value="[16,0,16,16,0,16]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
-   <property name="collidable" value="true"/>
    <property name="hitArea" value="[0,0,16,16,0,16]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="7">
   <properties>
-   <property name="collidable" value="true"/>
    <property name="hitArea" value="[0,0,16,0,16,16]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
  <tile id="8">
   <properties>
-   <property name="collidable" value="true"/>
    <property name="hitArea" value="[0,0,16,0,0,16]"/>
+   <property name="static" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="hitArea" value="[0,0,8,0,8,8,16,8,16,16,0,16]"/>
+   <property name="static" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="hitArea" value="[8,0,16,0,16,16,0,16,0,8,8,8]"/>
+   <property name="static" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="hitArea" value="[0,0,0,16,16,8,8,8,8,16,0,16]"/>
+   <property name="static" value="true"/>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="hitArea" value="[0,0,16,0,16,16,8,16,8,8,0,8]"/>
+   <property name="static" value="true"/>
    <property name="type" value="solid"/>
   </properties>
  </tile>
