ae90a45839dedcda9ae10cac32993f152ff01b6a
Chad Engler <englercj@live.com>
2013-08-03 14:34:21 -0700

    relative paths

--- js/game/data/resources.js
+++ js/game/data/resources.js
@@ -3,95 +3,95 @@ define([], function() {
         // Worlds
         {
             name: 'world_lightworld',
-            src: '/assets/worlds/lightworld/lightworld.json'
+            src: 'assets/worlds/lightworld/lightworld.json'
         },
         {
             name: 'world_linkshouse',
-            src: '/assets/worlds/linkshouse/linkshouse.json'
+            src: 'assets/worlds/linkshouse/linkshouse.json'
         },
         {
             name: 'world_darkworld',
-            src: '/assets/worlds/darkworld/darkworld.json'
+            src: 'assets/worlds/darkworld/darkworld.json'
         },
         // Sprites
         {
             name: 'sprite_link',
-            src: '/assets/sprites/entities/link.json'
+            src: 'assets/sprites/entities/link.json'
         },
         {
             name: 'sprite_smash',
-            src: '/assets/sprites/misc/smash.json'
+            src: 'assets/sprites/misc/smash.json'
         },
         {
             name: 'sprite_worlditems',
-            src: '/assets/worlds/shared_tiles/overworlditems.json'
+            src: 'assets/worlds/shared_tiles/overworlditems.json'
         },
         // Music
         {
             name: 'music_lightworld',
             src: [
-                '/assets/audio/music/overworld.lite.ogg'
+                'assets/audio/music/overworld.lite.ogg'
             ]
         },
         {
             name: 'music_darkworld',
             src: [
-                '/assets/audio/music/dark_world.lite.ogg'
+                'assets/audio/music/dark_world.lite.ogg'
             ]
         },
         {
             name: 'music_village',
             src: [
-                '/assets/audio/music/kakariko_village.lite.ogg'
+                'assets/audio/music/kakariko_village.lite.ogg'
             ]
         },
         // Sounds
         {
             name: 'effect_grass_cut',
             src: [
-                '/assets/audio/effects/LTTP_Grass_Cut.lite.ogg'
+                'assets/audio/effects/LTTP_Grass_Cut.lite.ogg'
             ]
         },
         {
             name: 'effect_lift',
             src: [
-                '/assets/audio/effects/LTTP_Link_Pickup.lite.ogg'
+                'assets/audio/effects/LTTP_Link_Pickup.lite.ogg'
             ]
         },
         {
             name: 'effect_throw',
             src: [
-                '/assets/audio/effects/LTTP_Link_Throw.lite.ogg'
+                'assets/audio/effects/LTTP_Link_Throw.lite.ogg'
             ]
         },
         {
             name: 'effect_open_chest',
             src: [
-                '/assets/audio/effects/LTTP_Chest.lite.ogg'
+                'assets/audio/effects/LTTP_Chest.lite.ogg'
             ]
         },
         {
             name: 'effect_smash',
             src: [
-                '/assets/audio/effects/LTTP_Shatter.lite.ogg'
+                'assets/audio/effects/LTTP_Shatter.lite.ogg'
             ]
         },
         //HUD
         {
             name: 'sprite_hud',
-            src: '/assets/sprites/ui/hud.json'
+            src: 'assets/sprites/ui/hud.json'
         },
         {
             name: 'font_retofganon',
-            src: '/assets/sprites/fonts/retofganon.json'
+            src: 'assets/sprites/fonts/retofganon.json'
         },
         {
             name: 'font_hud',
-            src: '/assets/sprites/fonts/hud.json'
+            src: 'assets/sprites/fonts/hud.json'
         },
         {
             name: 'sprite_life',
-            src: '/assets/ui/life.png'
+            src: 'assets/ui/life.png'
         }
     ];
 });
