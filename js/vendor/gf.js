e9bcd85f4531b301f2173418759eba2f04de8a48
Chad Engler <englercj@live.com>
2013-08-06 08:35:09 -0700

    new gf

--- js/vendor/gf.js
+++ js/vendor/gf.js
@@ -4,7 +4,7 @@
  * Copyright (c) 2012, Chad Engler
  * https://github.com/englercj/grapefruit
  *
- * Compiled: 2013-08-04
+ * Compiled: 2013-08-06
  *
  * GrapeFruit Game Engine is licensed under the MIT License.
  * http://www.opensource.org/licenses/mit-license.php
@@ -20683,11 +20683,12 @@ gf.inherits(gf.Camera, gf.DisplayObjectContainer, {
      * Adds a mask that will hide the world via a close-in transition.
      *
      * @method scanlines
-     * @param [shape='circle'] {String} The shape of the transition, either 'circle' or 'rectangle'
+     * @param [shape='circle'] {String} The shape of the transition, either 'circle' or 'rectangle' or 'ellipse'
      * @param [duration=1000] {Number} The time in milliseconds it takes to close the transition
+     * @param [position] {gf.Point|gf.Vector} The position to center on, defaults to the center of the screen
      * @return {gf.Camera.fx.Close} Returns the effect object
      */
-    close: function(shape, duration, cb) {
+    close: function(shape, duration, position, cb) {
         var close = this.fxpools.close.create(),
             self = this;
 
@@ -21033,16 +21034,23 @@ gf.Camera.fx.Close = function() {
 };
 
 gf.inherits(gf.Camera.fx.Close, gf.Camera.fx.Effect, {
-    start: function(shape, duration, cb) {
+    start: function(shape, duration, pos, cb) {
         gf.Camera.fx.Effect.prototype.start.call(this);
+
+        if(typeof pos ==='function') {
+            cb = pos;
+            pos = null;
+        }
 
         if(typeof duration === 'function') {
             cb = duration;
+            pos = null;
             duration = null;
         }
 
         if(typeof shape === 'function') {
             cb = shape;
+            pos = null;
             duration = null;
             shape = null;
         }
@@ -21051,19 +21059,24 @@ gf.inherits(gf.Camera.fx.Close, gf.Camera.fx.Effect, {
         this.duration = duration && duration > 0 ? duration : 1000;
         this.cb = cb;
 
-        if(shape === 'circle') {
-            this.cx = this.parent.size.x / 2;
-            this.cy = this.parent.size.y / 2;
-            this.radius = this.maxRadius = Math.max(this.parent.size.x / 2, this.parent.size.y / 2);
-        } else {
-            this.x = 0;
-            this.y = 0;
-            this.w = this.mx = this.parent.size.x;
-            this.h = this.my = this.parent.size.y;
-        }
+        this.cx = pos ? pos.x : this.parent.size.x / 2;
+        this.cy = pos ? pos.y : this.parent.size.y / 2;
+        this.w = this.mx = this.parent.size.x;
+        this.h = this.my = this.parent.size.y;
+        this.radius = this.maxRadius = Math.max(this.w / 2, this.h / 2);
 
         this.gfx.visible = true;
+        this.gfx.position.x = this.cx;
+        this.gfx.position.y = this.cy;
+
         this.parent.game.world.mask = this.gfx;
+
+        if(shape === 'ellipse') {
+            this.gfx.scale.y = 0.5;
+        }
+        else {
+            this.gfx.scale.y = 1;
+        }
 
         return this;
     },
@@ -21088,6 +21101,7 @@ gf.inherits(gf.Camera.fx.Close, gf.Camera.fx.Effect, {
         this.gfx.beginFill(0xff00ff);
 
         switch(this.shape) {
+            case 'ellipse':
             case 'circle':
                 this.radius -= (part * this.maxRadius);
 
@@ -21095,25 +21109,24 @@ gf.inherits(gf.Camera.fx.Close, gf.Camera.fx.Effect, {
                     this.stop();
                     this._complete();
                 } else {
-                    this.gfx.drawCircle(this.cx, this.cy, this.radius);
+                    this.gfx.drawCircle(0, 0, this.radius);
                 }
                 break;
 
             case 'rect':
             case 'rectangle':
-                this.x += (part * this.mx) / 2;
-                this.y += (part * this.my) / 2;
                 this.w -= (part * this.mx);
                 this.h -= (part * this.my);
 
-                if(this.x >= (this.mx / 2)) {
+                if(this.w <= 0) {
                     this.stop();
                     this._complete();
                 } else {
-                    this.gfx.drawRect(this.x, this.y, this.w, this.h);
+                    this.gfx.drawRect(-(this.w / 2), -(this.h / 2), this.w, this.h);
                 }
                 break;
         }
+        this.gfx.endFill();
 
         return this;
     }
@@ -23896,6 +23909,20 @@ gf.inherits(gf.TiledMap, gf.Map, {
 
             if(o.type === 'objectgroup') {
                 o.spawn();
+            }
+        }
+    },
+    /**
+     * Spawns all the objects in the TiledObjectGroups of this map
+     *
+     * @method spawnObjects
+     */
+    despawnObjects: function() {
+        for(var i = 0, il = this.children.length; i < il; ++i) {
+            var o = this.children[i];
+
+            if(o.type === 'objectgroup') {
+                o.despawn();
             }
         }
     },
