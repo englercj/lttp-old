fc986e28f660fba410ff9ad3439e7494333b81d6
Chad Engler <englercj@live.com>
2013-08-24 15:44:38 -0700

    fix link's house zone and flower animations

--- js/game/entities/misc/Flower.js
+++ js/game/entities/misc/Flower.js
@@ -6,18 +6,16 @@ define([
         Entity.call(this, gf.assetCache.sprite_misc, 0.2);
 
         this.addAnimation('flower_1', [
-            this.spritesheet['flower/flower_1_1.png'].frames[0],
-            this.spritesheet['flower/flower_1_2.png'].frames[0],
             this.spritesheet['flower/flower_1_3.png'].frames[0],
+            this.spritesheet['flower/flower_1_1.png'].frames[0],
             this.spritesheet['flower/flower_1_2.png'].frames[0]
-        ], 0.1, true);
+        ], 0.095, true);
 
         this.addAnimation('flower_2', [
-            this.spritesheet['flower/flower_2_1.png'].frames[0],
-            this.spritesheet['flower/flower_2_2.png'].frames[0],
             this.spritesheet['flower/flower_2_3.png'].frames[0],
+            this.spritesheet['flower/flower_2_1.png'].frames[0],
             this.spritesheet['flower/flower_2_2.png'].frames[0]
-        ], 0.1, true);
+        ], 0.095, true);
     };
 
     gf.inherits(Flower, Entity, {
