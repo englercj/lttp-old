aaf5597b98b1df5f4598229f2d4f4eb9e3a3fa08
Chad Engler <cengler@blizzard.com>
2013-11-04 10:33:26 -0800

    move and change build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#copy latest gf
+cp ../grapefruit/build/gf.js js/vendor/gf.js
+
+#copy latest gf-debug
+cp ../gf-debug/build/gf-debug.js js/vendor/gf-debug.js
+cp ../gf-debug/build/gf-debug.css css/gf-debug.css
+cp ../gf-debug/img/gamepad.png css/gamepad.png
--- js/vendor/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp ../grapefruit/build/gf.js js/vendor/gf.js
